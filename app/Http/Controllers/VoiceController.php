a3a5ca8da7a946432cbeef6129c938ab82a6be51
Event154 <nour.a@bacu.ae>
2025-11-26 18:04:37 +0200

    Update VoiceController.php

--- app/Http/Controllers/VoiceController.php
+++ app/Http/Controllers/VoiceController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -8,15 +9,24 @@ class VoiceController extends Controller
 {
     public function store(Request $request)
     {
+        // Check if audio exists
+        if(!$request->hasFile('audio')) {
+            return response()->json(['success' => false, 'message' => 'No audio found'], 400);
+        }
+
         $file = $request->file('audio');
 
+        // Insert in DB
         DB::table('voice_records')->insert([
-            'mime_type' => $file->getMimeType(),
+            'mime_type'  => $file->getMimeType(),
             'audio_data' => file_get_contents($file->getRealPath()),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
-        return response()->json(['message' => 'Record Saved Successfully 👌']);
+        return response()->json([
+            'success' => true,
+            'message' => 'Record Saved Successfully 🎉'
+        ]);
     }
 }
